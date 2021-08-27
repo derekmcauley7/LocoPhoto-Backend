f7b64b7815629a6865469f52f4c7b7aced27fe0f
Derek McAuley <derekmcauley7@gmail.com>
2021-08-27 11:57:31 +0100

    refactoring googlemaps class and adding logger

--- src/main/groovy/com/loco/photo/locophoto/tools/GoogleMaps.groovy
+++ src/main/groovy/com/loco/photo/locophoto/tools/GoogleMaps.groovy
@@ -1,61 +1,65 @@
 package com.loco.photo.locophoto.tools
 
-
-import com.google.code.geocoder.model.LatLng
+import ch.qos.logback.classic.Logger
 import org.apache.http.util.TextUtils
 import org.json.JSONArray
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
 
 class GoogleMaps {
-
+    
+    private static Logger logger = LoggerFactory.getLogger(GoogleMaps.class)
+    
     static String getCity(String lat, String lng) {
-
         String API_KEY = GoogleCredentials.API_KEY
-
         String cityName
-        LatLng latLng = new LatLng()
-        latLng.setLat(new BigDecimal(lat))
-        latLng.setLng(new BigDecimal(lng))
-
         String uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "2&key=" + API_KEY
-
         try {
             StringBuffer response = sendPlacesRequest(uri)
             cityName = getCityFromResponse(new JSONObject(response.toString()))
 
         } catch (Exception e) {
+            logger.error(e.toString())
             return null
         }
-
         return cityName
     }
 
     private static StringBuffer sendPlacesRequest(String uri) {
         StringBuffer response = new StringBuffer();
         try {
-            URL obj = new URL(uri)
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection()
-
-            con.setRequestMethod("GET")
-
-            con.setRequestProperty("User-Agent", "Mozilla/5.0")
-
-            BufferedReader bf = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()))
-            String inputLine
-
-            while ((inputLine = bf.readLine()) != null) {
-                response.append(inputLine)
-            }
-            bf.close()
-
+            BufferedReader bf = sendGetRequest(uri)
+            convertResponse(bf, response)
         } catch (Exception e) {
-            e.printStackTrace()
+            logger.error(e.printStackTrace())
         }
         return response
     }
 
-    static String getCityFromResponse(JSONObject jsonObj) {
+    private static void convertResponse(BufferedReader bf, StringBuffer response) {
+        String inputLine
+        while ((inputLine = bf.readLine()) != null) {
+            response.append(inputLine)
+        }
+        bf.close()
+    }
+
+    private static BufferedReader sendGetRequest(String uri) {
+        URL obj = new URL(uri)
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection()
+        con.setRequestMethod("GET")
+        con.setRequestProperty("User-Agent", "Mozilla/5.0")
+        BufferedReader bf = new BufferedReader(
+                new InputStreamReader(con.getInputStream()))
+        bf
+    }
+    
+    private static String setCityName(String longName, String city) {
+        city = longName
+        cleanName(city)
+    }
+    
+    protected static String getCityFromResponse(JSONObject jsonObj) {
         String cityName = ""
         JSONArray addressComponents = jsonObj.getJSONArray("results").getJSONObject(0).getJSONArray("address_components");
 
@@ -82,13 +86,8 @@ class GoogleMaps {
         cityName
     }
 
-    private static String setCityName(String longName, String city) {
-        city = longName
-        cleanName(city)
-    }
-
-    static String cleanName(String s){
-        return s.replaceAll("[0-9]","").trim()
+    protected static String cleanName(String s) {
+        return s.replaceAll("[0-9]", "").trim()
     }
 }
 
